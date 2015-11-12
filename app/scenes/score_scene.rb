27fd242f51c415c867ebf95d340ade15345b5bcd
Maciej Gajek <maltize@gmail.com>
2015-11-12 13:50:27 +0100

    update score scene

--- app/scenes/score_scene.rb
+++ app/scenes/score_scene.rb
@@ -1,9 +1,8 @@
-class ScoreScene < SKScene
+class ScoreScene < BaseScene
   def didMoveToView(view)
     super
 
     add_scores_titles(1000, 10000)
-    add_background
   end
 
   def add_scores_titles(your_score, high_score)
@@ -18,35 +17,5 @@ class ScoreScene < SKScene
     high_score_label.horizontalAlignmentMode = SKLabelHorizontalAlignmentModeLeft
     high_score_label.position = CGPointMake((max_x / 6), (max_y / 12) * 9)
     addChild high_score_label
-  end
-
-  def add_background
-    texture = SKTexture.textureWithImageNamed('numbers-bg.png')
-
-    background = SKSpriteNode.spriteNodeWithTexture(texture)
-    background.position = CGPointMake(mid_x, mid_y)
-    background.name = 'background'
-    background.zPosition = -20
-    background.scale = 1
-
-    addChild background
-  end
-
-  # Helper methods.
-  #
-  def mid_x
-    CGRectGetMidX(self.frame)
-  end
-
-  def mid_y
-    CGRectGetMidY(self.frame)
-  end
-
-  def max_x
-    CGRectGetMaxX(self.frame)
-  end
-
-  def max_y
-    CGRectGetMaxY(self.frame)
   end
 end
