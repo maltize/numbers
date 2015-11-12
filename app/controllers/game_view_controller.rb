b4649df4ca3db862b5b290246b521820a4a04097
Grzegorz Smajdor <grzegorz.smajdor@gmail.com>
2015-11-12 17:58:19 +0100

    background music

--- app/controllers/game_view_controller.rb
+++ app/controllers/game_view_controller.rb
@@ -19,10 +19,35 @@ class GameViewController < UIViewController
   def viewWillLayoutSubviews
     super
 
+    background_music
     unless self.view.scene
       scene = MenuScene.alloc.initWithSize(view.bounds.size)
       scene.scaleMode = SKSceneScaleModeAspectFill
       view.presentScene scene
     end
   end
+
+  def background_music
+    Dispatch::Queue.concurrent.async do
+      mainBundle = NSBundle.mainBundle
+      filePath = mainBundle.pathForResource("music", ofType:"mp3")
+      fileData = NSData.dataWithContentsOfFile(filePath)
+      error = Pointer.new(:object)
+
+      @audio_player = AVAudioPlayer.alloc.initWithData(fileData, error:error)
+      @audio_player.numberOfLoops = -1
+
+      unless (@audio_player.nil?)
+        @audio_player.delegate = self
+        if (@audio_player.prepareToPlay && @audio_player.play)
+          p "Successfully playing"
+        else
+          p "Failed to Play"
+        end
+      else
+        p "Failed to instantiate AVAudioPlayer"
+      end
+    end
+  end
+
 end
