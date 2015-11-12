414c802259e87be3d2ac85fe846f28eff6ed546e
Akshay Kumar <akshaykumar90@gmail.com>
2015-11-12 15:48:07 +0100

    Check for correct number on touch screen

--- app/scenes/game_scene.rb
+++ app/scenes/game_scene.rb
@@ -5,7 +5,7 @@ class GameScene < BaseScene
     super
 
     init_grid
-    puts @numbers
+
   end
 
   def init_grid
@@ -13,6 +13,28 @@ class GameScene < BaseScene
     num_blocks = self.view.difficulty + 2
     (0..31).to_a.sample(num_blocks).each_with_index do |position, number|
       @numbers << Number.new(number+1, position)
+    end
+  end
+
+  def touchesBegan(touches, withEvent: event)
+    touch = touches.anyObject
+    location = touch.locationInNode(self)
+    node = nodeAtPoint(location)
+    puts node.name
+
+    if @numbers.shift.value.to_s != node.name
+      scene = ScoreScene.alloc.initWithSize(self.view.bounds.size)
+      scene.scaleMode = SKSceneScaleModeAspectFill
+      self.view.presentScene scene
+    end
+
+    self.view.score += self.view.difficulty * 10
+
+    if @numbers.empty?
+      self.view.difficulty += 1
+      scene = GameScene.alloc.initWithSize(self.view.bounds.size)
+      scene.scaleMode = SKSceneScaleModeAspectFill
+      self.view.presentScene scene
     end
   end
 end
