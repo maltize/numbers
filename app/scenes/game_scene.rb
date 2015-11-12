dfb1d3e1586f09cda8bdbc02c819a7f0cdad9df5
Akshay Kumar <akshaykumar90@gmail.com>
2015-11-12 15:29:28 +0100

    Generate random positions for numbers

--- app/scenes/game_scene.rb
+++ app/scenes/game_scene.rb
@@ -1,17 +1,18 @@
 class GameScene < BaseScene
+  Number = Struct.new(:value, :position)
 
   def didMoveToView(view)
     super
 
-    add_title_label
+    init_grid
+    puts @numbers
   end
 
-  def add_title_label
-    label = SKLabelNode.labelNodeWithFontNamed("Zapfino")
-    label.text = "DaGame"
-    label.position = CGPointMake(mid_x, mid_y)
-    label.name = "dagame"
-    addChild label
+  def init_grid
+    @numbers = []
+    num_blocks = self.view.difficulty + 2
+    (0..31).to_a.sample(num_blocks).each_with_index do |position, number|
+      @numbers << Number.new(number+1, position)
+    end
   end
-
 end
